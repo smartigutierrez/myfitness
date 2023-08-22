45c5e216d751656817a800b2f6db6259037db4bb
sebastianmartig <sebastianmartig@gmail.com>
2023-08-22 18:25:15 -0400

    added comparison logic

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,4 +1,11 @@
 module WorkoutsHelper
-
-
+  def compare_attributes(attribute_name, value1, value2)
+    if value2.to_f > value1.to_f
+      content_tag(:p, "Improved #{attribute_name}: #{value2} (Previously: #{value1})")
+    elsif value2.to_f < value1.to_f
+      content_tag(:p, "Decreased #{attribute_name}: #{value2} (Previously: #{value1})")
+    else
+      content_tag(:p, "#{attribute_name} stayed the same: #{value2}")
+    end
+  end
 end
