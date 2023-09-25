119120e99b91eb1dee310d0372aff1bf8127dd3c
sebastianmartig <sebastianmartig@gmail.com>
2023-09-25 10:50:22 -0300

    added helpers & functional avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,8 +17,21 @@ module ApplicationHelper
     duration_in_minutes.map do |duration|
       display_time = start_time + duration * 60 # Convert minutes to seconds
       ["#{display_time.strftime('%H:%M')} (#{duration} mins)", duration]
+    end
   end
-end
 
+  def user_weight
+    (0..150).step(0.5).map { |weight| ["#{weight} kgs", weight] }
+  end
+
+  def user_height
+    (100..220).step(1).map { |height| ["#{height} cm", height] }
+  end
+
+def user_avatar(user, size = 15)
+  avatar = user.avatar if user.avatar.attached?
+
+  image_tag(avatar.presence || "avatar.jpg", size: "#{size}x#{size}!")
+end
 
 end
